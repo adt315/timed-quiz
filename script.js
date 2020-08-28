76bcf674c3a87ef3ecb7e92272656065c1b042c2
adt315 <angela_dyan@hotmail.com>
2020-08-27 20:11:53 -0400

    add navbar

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//  function answerCheck() {
+
+//   // Increase score if correct answer
+// //   if (document.querySelector(".correct").onclick) {
+//     //   document.querySelector(".correct").style.background-color="green";
+// //   }
+//   // document.getElementByClass('result-11').innerHTML = "Correct!";
+//   // score++;
+  
+// //   }
+
+//   btn.addEventListener("click", answerCheck);  
+    
+  //   else {
+  //     ("Wrong!");
+  //   }
+  
+
+  // // Show total at end
+  // alert("All done!  Your final score is " + score);
