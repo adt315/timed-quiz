9c8c69828077118d3030d44b8e1adcdb4e34896d
adt315 <angela_dyan@hotmail.com>
2020-08-28 11:17:16 -0400

    add more questions

---
+++ questions-javascript.js
@@ -0,0 +1,21 @@
+
+var questions = [ 
+    {
+        title: "How many states are smaller than Connecticut in total area?",
+        choices: [ "0", "1", "2", "3"],
+        answer: "2",
+    } , 
+    {
+        title: "What is the state bird of Connecticut?",
+        choices: ["American Robin", "Hermit Thrush", "Black-Capped Chickadee", "Purple Finch"],
+        answer: "American Robin"
+    } ,
+    {
+        title: "What is the state song of Connecticut?",
+        choices: [ "0", "1", "2", "3"],
+        answer: "2",
+    }
+]
+
+
+
--- script.js
+++ script.js
@@ -1,19 +1,75 @@
 
-//  function answerCheck() {
 
-// Increase score if correct answer
-//   if (document.querySelector(".correct").onclick) {
-//      document.querySelector(".correct").style.background-color="green";
-//   }
-//   document.getElementByClass("result").innerHTML = "Correct!";
-//   score++;
-//   }
 
-//   btn.addEventListener("click", answerCheck);  
-    
-//   else {
+// var questionsElement = document.getElementById("questions");
+// var choicesElement = document.getElementById("choices");
+
+
+// for(var i=0; i<questions.length; i++) {
+//   var currentQuestion = questions [i];
+//   console.log(currentQuestion);
+//   var titleElement = document.getElementById("question-title");
+//   titleElement.textContent = currentQuestion.title;
+//   questionsElement.append(titleElement);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// // Timer
+// var timeEl = document.querySelector(".time");
+// // var mainEl = document.getElementById("main");
+
+// var secondsLeft = 10;
+
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft;
+
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//     //   sendMessage();
+//     }
+
+//   }, 1000);
+// }
+
+// // function sendMessage() {
+// //   timeEl.textContent = " ";
+// // }
+
+// setTime();
+ 
+ 
+ 
+ 
+//  Score
+ 
+//  var score = 0;
+
+// function answerCheck() {
+// // Increase score if correct answer
+//     if (document.querySelector(".correct").onclick) {
+//     document.querySelector(".correct").style.background-color ="green";
+//     document.getElementByClass("result").innerHTML = "Correct!";
+//     score++;
+//   } else {
 //     ("Wrong!");
 //   }
-  
-// Show total at end
-// alert("All done!  Your final score is " + score);
+//  }
+
+
+// document.getElement
+// btn.addEventListener("click", answerCheck);  
+    
+// // // Show total at end
+// // alert("All done!  Your final score is " + score);
