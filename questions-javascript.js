22719b448546828ec7bb236c1727ef9c0bd8ef8e
adt315 <angela_dyan@hotmail.com>
2020-08-28 19:38:48 -0400

    create javascript questions object, add link

--- questions-javascript.js
+++ questions-javascript.js
@@ -2,19 +2,29 @@
 var questions = [ 
     {
         title: "How many states are smaller than Connecticut in total area?",
-        choices: [ "0", "1", "2", "3"],
+        choices: ["0", "1", "2", "3"],
         answer: "2",
     } , 
     {
         title: "What is the state bird of Connecticut?",
         choices: ["American Robin", "Hermit Thrush", "Black-Capped Chickadee", "Purple Finch"],
-        answer: "American Robin"
+        answer: "American Robin",
     } ,
     {
         title: "What is the state song of Connecticut?",
-        choices: [ "0", "1", "2", "3"],
-        answer: "2",
-    }
+        choices: ["Home On The Range", "These Green Mountains", "You Are My Sunshine", "Yankee Doodle"],
+        answer: "Yankee Doodle",
+    } ,
+    {
+        title: "What is the capital of Connecticut?",
+        choices: ["Bridgeport", "Hartford", "Springfield", "New Haven"],
+        answer: "Hartford",
+    } ,
+    {
+        title: "Connecticut was admitted as the _______ state.",
+        choices: ["second", "third", "fourth", "fifth"],
+        answer: "",
+    } ,
 ]
 
 
