487381baf162c2ccab313aa0f9e3155e18728849
adt315 <angela_dyan@hotmail.com>
2020-08-27 23:30:54 -0400

    add questions

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 
 //  function answerCheck() {
 
-//   // Increase score if correct answer
-// //   if (document.querySelector(".correct").onclick) {
-//     //   document.querySelector(".correct").style.background-color="green";
-// //   }
-//   // document.getElementByClass('result-11').innerHTML = "Correct!";
-//   // score++;
-  
-// //   }
+// Increase score if correct answer
+//   if (document.querySelector(".correct").onclick) {
+//      document.querySelector(".correct").style.background-color="green";
+//   }
+//   document.getElementByClass("result").innerHTML = "Correct!";
+//   score++;
+//   }
 
 //   btn.addEventListener("click", answerCheck);  
     
-  //   else {
-  //     ("Wrong!");
-  //   }
+//   else {
+//     ("Wrong!");
+//   }
   
-
-  // // Show total at end
-  // alert("All done!  Your final score is " + score);
+// Show total at end
+// alert("All done!  Your final score is " + score);
