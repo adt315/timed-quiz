a8d60b9330309be753b0e047afa01906ed6fd7df
adt315 <angela_dyan@hotmail.com>
2020-08-30 06:31:30 -0400

    subtract time for wrong answer

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 
-var start = document.getElementById("start-button");
-var startCard = document.getElementById("start-card");
-var question = document.getElementById("question");
-var question1 = document.getElementById("question1-card");
-var question2 = document.getElementById("question2-card");
-var question3 = document.getElementById("question3-card");
-var question4 = document.getElementById("question4-card");
-var question5 = document.getElementById("question5-card");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD= document.getElementById("D");
-var answerButton = document.getElementById("answer-button");
-var highScores= document.getElementById("high-scores-list");
-var timeElement = document.getElementById("time");
-// var = document.getElementById("");
+var start = document.querySelector("#start-button");
+var startCard = document.querySelector("#start-card");
+var question = document.querySelector(".question");
+var question1 = document.querySelector("#question1-card");
+var question2 = document.querySelector("#question2-card");
+var question3 = document.querySelector("#question3-card");
+var question4 = document.querySelector("#question4-card");
+var question5 = document.querySelector("#question5-card");
+var choices = document.querySelector(".choices");
+var choiceA = document.querySelector(".A-answer-btn");
+var choiceB = document.querySelector(".B-answer-btn");
+var choiceC = document.querySelector(".C-answer-btn");
+var choiceD = document.querySelector(".D-answer-btn");
+var highScores= document.querySelector("#high-scores-list");
+var timeElement = document.querySelector("#time");
 
 
 // var highScores = [];
@@ -68,31 +67,72 @@ var quizQuestions = [
 
 
 var secondsLeft = 76;
-
+var timerInterval;
+var quizQuestions = [question1, question2, question3, question4, question5];
+var questionNumber = 0;
 
 start.addEventListener("click", startQuiz);
 
 function startQuiz () {
-    function setTime() {
-        var timerInterval = setInterval(function() {
-          secondsLeft--;
-          timeElement.textContent = "Time: " + secondsLeft;
-      
-          if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            // quizEnd();
-          //   alert("Time's Up!");
-          }
-        }, 1000);
-      }
-      setTime();
+      timerInterval = setInterval(quizTimer, 1000);
       startCard.style.display="none";
       question1.style.display="block";
     //   getQuestion();
-
 }
 
-// answerBtn.addEventListener("click", getQuestion);
+function quizTimer() {
+    secondsLeft--;
+    timeElement.textContent = "Time: " + secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      // quizEnd();
+    //   alert("Time's Up!");
+    }
+  }
+
+  choiceA.addEventListener("click", choiceAResult);
+  choiceB.addEventListener("click", choiceBResult);
+  choiceC.addEventListener("click", choiceCResult);
+  choiceD.addEventListener("click", choiceDResult);
+  
+function choiceAResult () {
+    quizQuestions[questionNumber].style.display="none";
+    quizQuestions[questionNumber + 1].style.display="block";
+    questionNumber++;
+    if (choiceA.classList.contains("wrong")) {
+        secondsLeft -= 10;
+    }
+}
+
+function choiceBResult () {
+    quizQuestions[questionNumber].style.display="none";
+    quizQuestions[questionNumber + 1].style.display="block";
+    questionNumber++;
+    if (choiceB.classList.contains("wrong")) {
+        secondsLeft -= 10;
+    }
+}
+
+function choiceCResult () {
+    quizQuestions[questionNumber].style.display="none";
+    quizQuestions[questionNumber + 1].style.display="block";
+    questionNumber++;
+    if (choiceC.classList.contains("wrong")) {
+        secondsLeft -= 10;
+    }
+}
+
+function choiceDResult () {
+    quizQuestions[questionNumber].style.display="none";
+    quizQuestions[questionNumber + 1].style.display="block";
+    questionNumber++;
+    if (choiceD.classList.contains("wrong")) {
+        secondsLeft -= 10;
+    }
+}
+
+// answerButton.addEventListener("click", getQuestion);
 
 function answerCheck (answer) {
     if (quizQuestions[runningQuestionIndex].correct == answer){
@@ -111,7 +151,7 @@ function answerCheck (answer) {
 }
 
 // each button is a class called choice, we'll grab the value attribute to see if value = ..... check that with currentQuestion.answer
-// answerButton.addEventListener("click", answerCheck);
+answerButton.addEventListener("click", answerCheck);
 
 function correctAnswer () {
     // document.getElementById(runningQuestion).style.backgroundColor = "green";
@@ -135,32 +175,6 @@ function wrongAnswer () {
 
 
 // Don't think I need.../////////////////////////////////////////////////////////////////////////////////////////////////
-
-// var lastQuestionIndex = quizQuestions.length-1;
-// var runningQuestionIndex = 0;
-
-// function getQuestion () {
-    //question1.style.display="block";
-      
-    
-
-//     let q = quizQuestions[runningQuestionIndex];
-//     question.innerHTML = "<p>" + q.question + "</P>";
-//     choiceA.innerHTML = q.choiceA;
-//     choiceB.innerHTML = q.choiceB;
-//     choiceC.innerHTML = q.choiceC;
-//     choiceD.innerHTML = q.choiceD; 
-// }
-
-// getQuestion ();
-
-// runningQuestionIndex = 0;
-// getQuestion ()
-
-// runningQuestionIndex++
-// getQuestion ()
-
-
 
 
  // for(var i = 0; i < quizQuestions.length; i++) {
